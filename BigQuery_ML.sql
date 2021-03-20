3d1c956e5dca5d37813fae0600437ff0114f2f92
Abhimanyu Anand <63829842+abhimanyu729@users.noreply.github.com>
2021-03-20 12:38:33 -0400

    Update BigQuery_ML.sql

--- BigQuery_ML.sql
+++ BigQuery_ML.sql
@@ -21,7 +21,8 @@ WITH params AS (
 
   creditcard AS (
   SELECT
-    limit_balance AS balance, age, pay_0, pay_2, pay_3, pay_4, bill_amt_1, bill_amt_2, bill_amt_3, bill_amt_4, bill_amt_5, bill_amt_6, pay_amt_1, pay_amt_2, pay_amt_3, pay_amt_4, pay_amt_6, 
+    limit_balance AS balance, age, pay_0, pay_2, pay_3, pay_4, bill_amt_1, bill_amt_2, bill_amt_3, bill_amt_4, bill_amt_5, bill_amt_6, pay_amt_1,
+    pay_amt_2, pay_amt_3, pay_amt_4, pay_amt_6, 
     CAST(default_payment_next_month AS INT64) AS default_payment_next_month
     FROM
     `bigquery-public-data.ml_datasets.credit_card_default` , params
@@ -52,7 +53,8 @@ WITH params AS (
 
   creditcard AS (
   SELECT
-    limit_balance AS balance, age, pay_0, pay_2, pay_3, pay_4, bill_amt_1, bill_amt_2, bill_amt_3, bill_amt_4, bill_amt_5, bill_amt_6, pay_amt_1, pay_amt_2, pay_amt_3, pay_amt_4, pay_amt_6, 
+    limit_balance AS balance, age, pay_0, pay_2, pay_3, pay_4, bill_amt_1, bill_amt_2, bill_amt_3, bill_amt_4, bill_amt_5, 
+    bill_amt_6, pay_amt_1, pay_amt_2, pay_amt_3, pay_amt_4, pay_amt_6, 
     CAST(default_payment_next_month AS INT64) AS default_payment_next_month
     FROM
     `bigquery-public-data.ml_datasets.credit_card_default` , params
@@ -85,7 +87,10 @@ WITH params AS (
 
   creditcard AS (
   SELECT
-    limit_balance AS balance, age, (pay_0 + pay_2 + pay_3 + pay_4 + CAST( pay_5 AS INT64 ) + CAST( pay_6 AS INT64)) AS pay , (bill_amt_1 + bill_amt_2 + bill_amt_3 + bill_amt_4 + bill_amt_5 + bill_amt_6) as bill, (pay_amt_1 + pay_amt_2 + pay_amt_3 + pay_amt_4 + pay_amt_5 + pay_amt_6) as pay_amt, CAST(default_payment_next_month AS INT64) AS default_payment_next_month, CAST( sex AS INT64) AS SEX, CAST( education_level AS INT64) education, CAST(marital_status AS INT64) AS marital_status
+    limit_balance AS balance, age, (pay_0 + pay_2 + pay_3 + pay_4 + CAST( pay_5 AS INT64 ) + CAST( pay_6 AS INT64)) AS pay , 
+    (bill_amt_1 + bill_amt_2 + bill_amt_3 + bill_amt_4 + bill_amt_5 + bill_amt_6) as bill, (pay_amt_1 + pay_amt_2 + pay_amt_3 + pay_amt_4 + pay_amt_5 + pay_amt_6) as pay_amt, 
+    CAST(default_payment_next_month AS INT64) AS default_payment_next_month, CAST( sex AS INT64) AS SEX, CAST( education_level AS INT64) education, 
+    CAST(marital_status AS INT64) AS marital_status
     FROM
     `bigquery-public-data.ml_datasets.credit_card_default` , params
     WHERE 
@@ -118,7 +123,10 @@ WITH params AS (
 
   creditcard AS (
   SELECT
-    limit_balance AS balance, age, (pay_0 + pay_2 + pay_3 + pay_4 + CAST( pay_5 AS INT64 ) + CAST( pay_6 AS INT64)) AS pay , (bill_amt_1 + bill_amt_2 + bill_amt_3 + bill_amt_4 + bill_amt_5 + bill_amt_6) as bill, (pay_amt_1 + pay_amt_2 + pay_amt_3 + pay_amt_4 + pay_amt_5 + pay_amt_6) as pay_amt, CAST(default_payment_next_month AS INT64) AS default_payment_next_month, CAST( sex AS INT64) AS SEX, CAST( education_level AS INT64) education, CAST(marital_status AS INT64) AS marital_status
+    limit_balance AS balance, age, (pay_0 + pay_2 + pay_3 + pay_4 + CAST( pay_5 AS INT64 ) + CAST( pay_6 AS INT64)) AS pay , 
+    (bill_amt_1 + bill_amt_2 + bill_amt_3 + bill_amt_4 + bill_amt_5 + bill_amt_6) as bill, (pay_amt_1 + pay_amt_2 + pay_amt_3 + pay_amt_4 + pay_amt_5 + pay_amt_6) as pay_amt, 
+    CAST(default_payment_next_month AS INT64) AS default_payment_next_month, CAST( sex AS INT64) AS SEX, CAST( education_level AS INT64) education, 
+    CAST(marital_status AS INT64) AS marital_status
     FROM
     `bigquery-public-data.ml_datasets.credit_card_default` , params
     WHERE 
